dd1a59150080c709cd31631542f1e32e7db320e2
vitaliksmile <vitaliksmile777@gmail.com>
2021-10-01 18:35:12 +0300

    PR fixes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import logo from './logo.svg';
+import './App.css';
 export default function App() {
   return (
     <Router>
